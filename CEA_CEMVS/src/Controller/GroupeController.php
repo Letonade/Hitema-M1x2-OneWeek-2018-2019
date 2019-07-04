f303d00e68b5f5e68b900c038ce2ae1ccb56fce2
nanaiori <eljaafarii.Najlaa@gmail.com>
2019-07-04 10:24:05 +0200

    recup

--- CEA_CEMVS/src/Controller/GroupeController.php
+++ CEA_CEMVS/src/Controller/GroupeController.php
@@ -1,22 +1,21 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\TireurGroupe;
+use App\Entity\User;
 use App\Form\TireurGroupeType;
 use App\Repository\TireurGroupeRepository;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 /**
- * @Route("/Tireurgroupe")
+ * @Route("/groupe")
  */
-class TireurGroupeController extends AbstractController
+class GroupeController extends AbstractController
 {
     /**
-     * @Route("/", name="groupeIndex", methods={"GET"})
+     * @Route("/", name="groupe_index", methods={"GET"})
      */
     public function index(TireurGroupeRepository $TireurGroupeRepository): Response
     {
@@ -24,73 +23,73 @@ class TireurGroupeController extends AbstractController
             'groupes' => $TireurGroupeRepository->findAll(),
         ]);
     }
-
     /**
-     * @Route("/newGroupe", name="newGroupe", methods={"GET","POST"})
+     * @Route("/new", name="groupe_new", methods={"GET","POST"})
      */
-    public function newGroupe(Request $request): Response
+    public function new(Request $request): Response
     {
         $groupe = new TireurGroupe();
         $form = $this->createForm(TireurGroupeType::class, $groupe);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
+            $tireurs = $groupe->getTireurs();
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($groupe);
+            foreach($tireurs as $tireur){
+                $tireur->setGroupe($groupe);
+            }
+            $entityManager->persist($tireur);
             $entityManager->flush();
-
-            return $this->redirectToRoute('groupeIndex');
+            return $this->redirectToRoute('groupe_index');
         }
-
-        return $this->render('groupe/newGroupe.html.twig', [
+        return $this->render('groupe/new.html.twig', [
             'groupe' => $groupe,
             'form' => $form->createView(),
         ]);
     }
-
     /**
-     * @Route("/{id}", name="groupeShow", methods={"GET"})
+     * @Route("/{id}", name="groupe_show", methods={"GET"})
      */
-    public function show(TireurGroupe $groupe): Response
+    public function show(Groupe $groupe): Response
     {
-        return $this->render('groupe/showgroupe.html.twig', [
+        return $this->render('groupe/show.html.twig', [
             'groupe' => $groupe,
         ]);
     }
-
     /**
-     * @Route("/{id}/edit", name="groupeEdit", methods={"GET","POST"})
+     * @Route("/{id}/edit", name="groupe_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, TireurGroupe $groupe): Response
+    public function edit(Request $request, Groupe $groupe): Response
     {
-        $form = $this->createForm(TireurGroupeType::class, $groupe);
+        $form = $this->createForm(GroupeType::class, $groupe);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
+            $tireurs = $groupe->getTireurs();
+            foreach($tireurs as $tireur){
+                $tireur->setGroupe($groupe);
+            }
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($tireur);
             $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('groupeIndex', [
+            return $this->redirectToRoute('groupe_index', [
                 'id' => $groupe->getId(),
             ]);
         }
-
         return $this->render('groupe/edit.html.twig', [
             'groupe' => $groupe,
             'form' => $form->createView(),
         ]);
     }
-
     /**
-     * @Route("/{id}", name="groupeDelete", methods={"DELETE"})
+     * @Route("/{id}", name="groupe_delete", methods={"DELETE"})
      */
-    public function delete(Request $request, TireurGroupe $groupe): Response
+    public function delete(Request $request, Groupe $groupe): Response
     {
         if ($this->isCsrfTokenValid('delete'.$groupe->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($groupe);
             $entityManager->flush();
         }
-
-        return $this->redirectToRoute('groupeIndex');
+        return $this->redirectToRoute('groupe_index');
     }
 }
--- CEA_CEMVS/src/Form/TireurGroupeType.php
+++ CEA_CEMVS/src/Form/TireurGroupeType.php
@@ -1,22 +1,33 @@
 <?php
 namespace App\Form;
 use App\Entity\TireurGroupe;
+use App\Entity\User;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 class TireurGroupeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
-            ->add('entrainements')
-        ;
+            ->add('name')
+            ->add('tireurs',EntityType::class,array(
+                "class" => User::class,
+                "choice_label"=>"prenom",
+                "multiple"=>TRUE,
+                "required" => false,
+                "query_builder"=>function(EntityRepository $er)
+                {
+                    return $er->createQueryBuilder('t')->where('t.groupe is null');
+                }
+            ));
     }
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Groupe::class,
+            'data_class' => TireurGroupe::class,
         ]);
     }
 }
